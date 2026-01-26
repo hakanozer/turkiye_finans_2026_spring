c417e625a9c63373a1de30c659f31f62042699a7
Hakan ÖZER <hakanozer02@gmail.com>
2026-01-26 09:51:00 +0300

    Project Start

---
+++ src/main/java/com/works/turkiye_finans_2026_spring/TurkiyeFinans2026SpringApplication.java
@@ -0,0 +1,13 @@
+package com.works.turkiye_finans_2026_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurkiyeFinans2026SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TurkiyeFinans2026SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/works/turkiye_finans_2026_spring/TurkiyeFinans2026SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.works.turkiye_finans_2026_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurkiyeFinans2026SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
